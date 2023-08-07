635743d6522d09350ad1a3201401d1120216198a
agarwalayush9 <agarwalayush991@gmail.com>
2023-08-07 12:55:33 +0530

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -35,8 +35,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        process.env.VITE_APP_EMAILJS_SERVICE_ID,
+       process.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           to_name: "Ayush",
@@ -44,7 +44,7 @@ const Contact = () => {
           to_email: "ayushag.cse@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        process.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
