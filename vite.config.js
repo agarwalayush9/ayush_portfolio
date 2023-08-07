668bfe63bad37d1bc8373de18e8b2c05d94f234f
agarwalayush9 <agarwalayush991@gmail.com>
2023-08-07 14:13:09 +0530

    VITE

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.VITE_APP_EMAILJS_PUBLIC_KEY':JSON.stringify(process.env.VITE_APP_EMAILJS_PUBLIC_KEY),
+    'process.env.VITE_APP_EMAILJS_SERVICE_KEY':JSON.stringify(process.env.VITE_APP_EMAILJS_SERVICE_KEY),
+    'process.env.VITE_APP_EMAILJS_TEMPLATE_KEY':JSON.stringify(process.env.VITE_APP_EMAILJS_TEMPLATE_KEY)
+  }
 })
